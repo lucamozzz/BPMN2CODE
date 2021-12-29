bdc2298047c29ad987f8703284913f75c5e84a34
alessandrotesta-99 <alessandro.testa@studenti.unicam.it>
2021-12-29 20:13:08 +0100

    change test.bpmn

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from BPMNParser import BPMNParser
 
 
 if __name__ == '__main__':
-    parser = BPMNParser('C:\\Users\\alessandro\\Documents\\PycharmProjects-workspace\\BPMN2CODE\\test.bpmn')
+    parser = BPMNParser('test.bpmn')
     parser.parse_nodes()
     parser.connect_nodes()
     parser.tree.build_tree()
