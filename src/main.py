712057fcaed61b10af76e8bf4428bb94e42be612
Luca Mozzoni <mozzonilc@gmail.com>
2022-03-04 16:11:13 +0100

    Allow input from command line

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from TranslateAlgorithm import TranslateAlgorithm
 from Validator import Validator
 
 if __name__ == '__main__':
-    # parser = BPMNParser(sys.argv[1])
-    parser = BPMNParser("test.bpmn")
+    parser = BPMNParser(sys.argv[1])
+    # ™parser = BPMNParser("test.bpmn")
     parser.parse_nodes()
     raw_tree = parser.connect_nodes()
     validator = Validator(parser)
